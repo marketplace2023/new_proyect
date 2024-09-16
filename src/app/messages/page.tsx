6cb57957d7e78deef331f5963d461a0c00989c93
marketplace2023 <mpcourses2022@gmail.com>
2024-09-16 10:02:48 -0400

    cambios

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import { Button } from '@/components/ui/button';
+
 const MessagesPage = () => {
   return (
     <>
       <span className="font-bold text-4xl">Messages</span>
+      <div className="flex justify-end">
+        <Button>Ingresar Nuevo Cliente</Button>
+      </div>
 
       <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
       <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
