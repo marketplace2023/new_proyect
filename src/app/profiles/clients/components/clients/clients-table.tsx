8b2315448dddc1cc561ba130fe1aac6a302cac5e
marketplace2023 <mpcourses2022@gmail.com>
2024-09-17 16:55:29 -0400

    discard changes

--- src/app/profiles/clients/components/clients/clients-table.tsx
+++ src/app/profiles/clients/components/clients/clients-table.tsx
@@ -1,10 +1,41 @@
-import { DataTable } from '@/components/data-table/data-table';
+import React from 'react';
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 
 import { clients } from '../../api/data';
-import { columns } from './colums';
 
 const ClientsTable = () => {
-  return <DataTable columns={columns} data={clients} />;
+  return (
+    <Table>
+      <TableCaption>A list of your recent clients.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Name Client</TableHead>
+          <TableHead>Phone Number</TableHead>
+          <TableHead>Email</TableHead>
+          <TableHead>Password</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {clients.map((client, index) => (
+          <TableRow key={index}>
+            <TableCell>{client.name}</TableCell>
+            <TableCell>{client.phoneNumber}</TableCell>
+            <TableCell>{client.email}</TableCell>
+            <TableCell>{client.password}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 };
 
 export default ClientsTable;
